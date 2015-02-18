801946d825696b089d10c6ea63cabfdc91300f93
Ubuntu <ubuntu@ip-172-31-27-53.us-west-2.compute.internal>
2015-02-18 00:32:53 +0000

    change local port

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ http.globalAgent.maxSockets = Infinity;
 var web = require('./lib/web');
 
 var RABBIT_URL = process.env.CLOUDAMQP_URL || 'amqp://localhost';
-var PORT = process.env.PORT || 5000;
+var PORT = process.env.PORT || 8080;
 var SERVICE_TIME = process.env.SERVICE_TIME || 500;
 
 throng(start, { workers: cpus, lifetime: Infinity });
